9a8b190f63c08b5e1d504232dba727a5d105c22a
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-06-20 11:54:47 +0200

    create uesrs model

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    bio = db.Column(db.Text)
+    avatar_url = db.Column(db.String(255))
